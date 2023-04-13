6ab9d1c4f2c3f8a99752207fd9f6946ff6514e25
Severino Vinicius <severino.sales95@gmail.com>
2023-04-13 18:50:16 -0300

    Requisito 2 Realizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { createUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -72,5 +73,11 @@ class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
