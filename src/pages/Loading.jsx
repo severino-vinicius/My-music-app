b732735173cbc4c8dac6901275fee39932b5dad6
Severino Vinicius <severino.sales95@gmail.com>
2023-04-13 18:40:20 -0300

    Requisito 2 com erro de lint

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class IsLoading extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Carregando...
+        </h1>
+      </div>
+    );
+  }
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
+  state = {
+    name: '',
+    saveBtnDisabbled: true,
+    isLoading: false,
+  };
+
+  verifyInput = () => {
+    const { name } = this.state;
+    const minChars = 3;
+    if (name.length >= minChars) {
+      this.setState({
+        saveBtnDisabbled: false,
+      });
+    } else {
+      this.setState({
+        saveBtnDisabbled: true,
+      });
+    }
+  };
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    }, this.verifyInput);
+  };
+
+  onLoginClick = async () => {
+    const { history } = this.props;
+    const { name } = this.state;
+    const user = {
+      name,
+    };
+    this.setState({
+      isLoading: true,
+    });
+    await createUser(user);
+    history.push('/search');
+  };
+
   render() {
+    const { saveBtnDisabbled, isLoading } = this.state;
     return (
       <div data-testid="page-login">
-        Login
+        <h1>Login</h1>
+        { isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <input
+              type="text"
+              placeholder="Nome"
+              data-testid="login-name-input"
+              onChange={ this.onInputChange }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ saveBtnDisabbled }
+              onClick={ this.onLoginClick }
+            >
+              Entrar
+            </button>
+          </>
+        )}
       </div>
     );
   }
