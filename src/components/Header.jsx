efdd2e1f21d071951d0dac4dcae3648a2475daa3
Severino Vinicius <severino.sales95@gmail.com>
2023-04-14 08:30:53 -0300

    Requisito 4 realizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -27,6 +28,11 @@ class Header extends Component {
     const { name, isLoading } = this.state;
     return (
       <header data-testid="header-component">
+        <nav>
+          <Link to="/search" data-testid="link-to-search"> Search </Link>
+          <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
+          <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
+        </nav>
         { isLoading ? (
           <Loading />
         ) : (
